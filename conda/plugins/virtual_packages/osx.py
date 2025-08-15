c97b5685197347c1d98016f6dda52ac1d6f54759
Cheng H. Lee <clee@anaconda.com>
2025-08-15 11:06:52 -0500

    [POC] Expose `__rosetta` virtual package

--- conda/plugins/virtual_packages/osx.py
+++ conda/plugins/virtual_packages/osx.py
@@ -8,6 +8,22 @@ from ...base.context import context
 from .. import CondaVirtualPackage, hookimpl
 
 
+def _detect_rosetta():
+    cpu_vendor, cpu_model = "", ""
+    try:
+        from archspec.cpu.detect import CpuidInfoCollector
+        cpu = CpuidInfoCollector()
+        cpu_vendor = cpu.vendor
+        cpu_model = cpu.brand_string()
+    except (ImportError, SystemError):
+        cpu_vendor, cpu_model = "", ""
+
+    # Not 100% sure we really _need_ to check the CPU vendor string, but I
+    # figure better safe than sorry, since certain AMD CPUs let users modify
+    # the model/brand string.
+    return (cpu_vendor == "GenuineIntel" and cpu_model == "VirtualApple")
+
+
 @hookimpl
 def conda_virtual_packages():
     if not context.subdir.startswith("osx-"):
@@ -27,3 +43,8 @@ def conda_virtual_packages():
     if dist_version:  # truthy override found
         yield CondaVirtualPackage("osx", dist_version, None)
     # if a falsey override was found, the __osx virtual package is not exported
+
+    # 3: determine if we're running in Rosetta
+    if context.subdir == "osx-64" and _detect_rosetta():
+        # Version 2 since Rosetta 1 covered the PPC -> x86 transition
+        yield CondaVirtualPackage(f"rosetta", "2", "0")
